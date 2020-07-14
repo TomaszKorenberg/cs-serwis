e7ad2cf5daa842700556f655c6593d0b25b61b08
Tomasz Korenberg <info@casestudio.pl>
2020-07-14 02:26:52 +0200

    backend changes

--- api/index.js
+++ api/index.js
@@ -3,10 +3,17 @@ const cors = require("cors");
 const app = express();
 const sequelize = require('./utils/database');
 const config = require('./config');
+const path = require('path');
+
 
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 require('./routes/clients')(app);
 require('./routes/repairs')(app);
--- api/routes/clients.js
+++ api/routes/clients.js
@@ -4,13 +4,13 @@ const Client = require("./../models/client");
 module.exports = (app) => {
     app.post("/clients/addclient", (req, res) => {
         Client.create({
-            name: "Imię",      //todo:podminić na dane wysyłane z frontendu
-            lastName: "Nazwisko",
-            contactNumber: "Numer kontaktowy",
-            email: "e-m@il",
-            streetAdress: "Ulica i numer",
-            postalCode: "Kod-pocztowy",
-            city: "Miasto"
+            name: req.body.firstName,
+            lastName: req.body.lastName,
+            contactNumber: req.body.phoneNumber,
+            email: req.body.email,
+            streetAdress: req.body.adress, //do poprawy aby front przesyłał rozbity adres
+            postalCode: null,
+            city: null
         })
             .then(() => {
                 res.status(200).send("Created new client")
