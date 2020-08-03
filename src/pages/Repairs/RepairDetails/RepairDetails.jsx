08d20ab6db358e86b85b03b2e6eb82c9a1d4aaf7
Tomasz Korenberg <info@casestudio.pl>
2020-08-03 02:25:28 +0200

    RepairDetails component second sketch

--- src/pages/Repairs/RepairDetails/RepairDetails.jsx
+++ src/pages/Repairs/RepairDetails/RepairDetails.jsx
@@ -4,9 +4,6 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import {getRepairById} from "../../../redux/operations";
 import {useDispatch, useSelector} from "react-redux";
 import "./RepairDetails.scss"
-import ClientDetails from "./ClientDetails/ClientDetails";
-import DeviceDetails from "./DeviceDetails/DeviceDetails";
-import RepairActions from "./RepairActions/RepairActions";
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
@@ -15,6 +12,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Button from "@material-ui/core/Button";
+import Divider from '@material-ui/core/Divider';
 
 
 function TabPanel(props) {
@@ -102,20 +101,22 @@ export default function RapairDetails() {
                             <li><b>Producent:</b> {repairDetails.device.manufacturer}</li>
                             <li><b>Model:</b> {repairDetails.device.model}</li>
                             <li><b>S/N:</b> {repairDetails.serialNumber}</li>
+                            <li><b>Data przyjęcia:</b> {repairDetails.dateOfAdd}</li>
                         </ul>
                         <ul>
                             <li><b>Klient:</b> {repairDetails.client.name} {repairDetails.client.lastName}</li>
-                            <li><b>Numer do klienta:</b> {repairDetails.client.contactNumber ? repairDetails.client.contactNumber : "Brak"}</li>
-                            <li><b>Email do klienta:</b> {repairDetails.client.email ? repairDetails.client.email : "Brak"}</li>
-                            <li><b>Adres:</b> {repairDetails.client.streetAdress}<br/>{repairDetails.client.postalCode} {repairDetails.client.city}</li>
-                            <li><b>Data przyjęcia:</b> {repairDetails.dateOfAdd}</li>
+                            <li><b>Numer do
+                                klienta:</b> {repairDetails.client.contactNumber ? repairDetails.client.contactNumber : "Brak"}
+                            </li>
+                            <li><b>Email do
+                                klienta:</b> {repairDetails.client.email ? repairDetails.client.email : "Brak"}</li>
+                            <li>
+                                <b>Adres:</b> {repairDetails.client.streetAdress}<br/>{repairDetails.client.postalCode} {repairDetails.client.city}
+                            </li>
 
                         </ul>
                     </div>
                 </div>
-
-                {/*<DeviceDetails repairDetails={repairDetails}/>*/}
-                {/*<ClientDetails repairDetails={repairDetails}/>*/}
 
             </div>
 
@@ -141,11 +142,127 @@ export default function RapairDetails() {
                 onChangeIndex={handleChangeIndex}
             >
                 <TabPanel value={value} index={0} dir={theme.direction}>
+                    Status:<br/>
+                    - Przyjęto do serwisu/Oczekuje na rozpoczęcie naprawy<br/>
+                    <div className={"repairActionsWrapper"}>
 
+                        {(repairDetails.status === "new")
+                            ? <div>
+                                Naprawa ma status "Przyjęto do serwisu".<br/>
+                                <Button variant="outlined">Rozpocznij obsługę zlecenia</Button><br/><br/>
+
+                                Inne dostępne operacje:<br/>
+                                <Button variant="outlined">Dodaj komentarz</Button>
+                                {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                                    ? <Button variant="outlined">Wiadomość do klienta</Button>
+                                    : null}
+                                <Button variant="outlined">Zakończ bez naprawy</Button>
+                            </div>
+                            : null}
+
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - Rozpoczęto naprawę/Twoja naprawa rozpoczęła się i oczekuje na diagnozę<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                            <Button variant="outlined">Dodaj ekspertyzę</Button><br/><br/>
+                            Inne dostępne operacje:<br/>
+                            <Button variant="outlined">Dodaj komentarz</Button>
+                            {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                                ? <Button variant="outlined">Wiadomość do klienta</Button>
+                                : null}
+                            <Button variant="outlined">Zakończ bez naprawy</Button>
+                        </div>
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - Ekspertyza/ Technik wycenił naprawę. Twoj ruch, czekamy na decyzję<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                            <Button variant="outlined">Rozpocznij naprawę</Button><br/><br/>
+                            Inne dostępne operacje:<br/>
+                            <Button variant="outlined">Dodaj komentarz</Button>
+                            {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                                ? <Button variant="outlined">Wiadomość do klienta</Button>
+                                : null}
+                            <Button variant="outlined">Zakończ bez naprawy</Button>
+                        </div>
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - W trakcie naprawy / Technik właśnie naprawia urządzenie<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                        <Button variant="outlined">Zamowione części</Button><br/>
+                        <Button variant="outlined">Przekazanie naprawy</Button><br/><br/>
+                        Inne dostępne operacje:<br/>
+                        <Button variant="outlined">Dodaj komentarz</Button>
+                        {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                            ? <Button variant="outlined">Wiadomość do klienta</Button>
+                            : null}
+                        <Button variant="outlined">Zakończ bez naprawy</Button>
+                    </div>
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - Oczekująca / Twoja naprawa oczekuje<br/>
+                    /Oczekuje na części<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                        <Button variant="outlined">Przyszły części</Button><br/>
+                        <Button variant="outlined">Naprawiono</Button><br/><br/>
+                        Inne dostępne operacje:<br/>
+                        <Button variant="outlined">Dodaj komentarz</Button>
+                        {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                            ? <Button variant="outlined">Wiadomość do klienta</Button>
+                            : null}
+                        <Button variant="outlined">Zakończ bez naprawy</Button>
+                    </div>
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - Oczekująca / Twoja naprawa oczekuje<br/>
+                    /Przekazano do serwisu zewnętrznego (widziane tylko dla serwisu, dla klienta oczekuje na
+                    części)<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                        <Button variant="outlined">Powrot sprzętu z serwisu</Button><br/><br/>
+                        Inne dostępne operacje:<br/>
+                        <Button variant="outlined">Dodaj komentarz</Button>
+                        {(repairDetails.client.email || repairDetails.client.phoneNumber)
+                            ? <Button variant="outlined">Wiadomość do klienta</Button>
+                            : null}
+                        <Button variant="outlined">Zakończ bez naprawy</Button>
+                    </div>
+                    </div>
+                    <Divider/>
+
+
+                    Status:<br/>
+                    - Naprawa zakończona / Możesz odebrac urządzenie<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                        <Button variant="outlined">Odbior sprzętu przez klienta</Button><br/>
+                        </div>
+                    </div>
+                    <Divider/>
+
+                    Status:<br/>
+                    - Odebrane / To tyle z naszej strony (oby nie) do zobaczenia! :)<br/>
+                    <div className={"repairActionsWrapper"}>
+                        <div>
+                        </div>
+                    </div>
+                    <Divider/>
 
                 </TabPanel>
                 <TabPanel value={value} index={1} dir={theme.direction}>
-                    <RepairActions repairDetails={repairDetails}/>
                     Data rozpoczęcia i data zakończenia naprawy:<br/>
                     Diagnoza i opis:<br/>
                     Części:<br/>
@@ -153,8 +270,18 @@ export default function RapairDetails() {
 
                 </TabPanel>
                 <TabPanel value={value} index={2} dir={theme.direction}>
-                    Rozliczenie:<br/>
+                    Dane klienta:<br/>
+                    Dane urządzenia:
 
+                </TabPanel>
+                <TabPanel value={value} index={3} dir={theme.direction}>
+                    Zużyte części:<br/>
+                    Czas poświęcony na naprawę:
+                </TabPanel>
+                <TabPanel value={value} index={4} dir={theme.direction}>
+                    - Dnia 1.1.1111r przyjęto urządzenie do serwisu<br/>
+                    - Dnia 1.1.1111r technik rozpoczął pomiary<br/>
+                    - Dnia 1.1.1111r technik wydał werdykt<br/>
                 </TabPanel>
             </SwipeableViews>
         </div>
