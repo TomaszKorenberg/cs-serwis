73d724930609c98a7238cf3f76ae818f6801d0b4
Tomasz Korenberg <info@casestudio.pl>
2020-08-08 00:25:32 +0200

    New table with repairs

--- src/pages/Repairs/Repairs.jsx
+++ src/pages/Repairs/Repairs.jsx
@@ -1,29 +1,48 @@
 import React, {useEffect} from 'react';
+import MaterialTable from 'material-table';
+import {forwardRef} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import {getAllRepairs} from "../../redux/operations";
 import {leftMenuActions} from "../../redux/actions/index"
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import {useHistory} from "react-router-dom";
 import moment from "moment";
-import Repairs2 from "./Repairs2";
+import {useHistory} from "react-router-dom";
 
 
-const columns = [
-    {id: 'repairId', label: 'ID:', minWidth: 25},
-    {id: 'manufacturer', label: 'Producent', minWidth: 100},
-    {id: 'model', label: 'Model', minWidth: 100},
-    {id: 'serialNumber', label: 'Numer seryjny', minWidth: 100},
-    {id: 'faultDescription', label: 'Opis usterki', minWidth: 300, align: 'left'},
-    {id: 'dateOfAdd', label: 'Data przyjęcia', minWidth: 100},
-    {id: 'status', label: 'Status', minWidth: 100}
-];
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+};
 
 const leftMenuItems = [
     {path: "/repairs", text: "Aktualne naprawy"},
@@ -33,166 +52,102 @@ const leftMenuItems = [
     {path: "/repairs/faults", text: "Baza usterek"},
 ];
 
-const Repairs = () => {
+
+export default function Repairs2() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const repairs = useSelector(state => state.repairs);
 
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(100);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+    useEffect(() => {
+        dispatch(getAllRepairs());
+        leftMenuActions.setLeftMenu([...leftMenuItems]);
 
-    const handleRapairClick = (repairId) => {
+    }, [dispatch]);
+
+
+
+    const handleOnRowClick = (repairId) => {
         history.push("/repairs/repair-" + repairId)
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
+
+    const tableConfiguration = {
+
+        columns: [
+            {field: 'repairId', title: 'ID:'},
+            {field: 'device.manufacturer', title: "Producent"},
+            {field: 'device.model', title: 'Model'},
+            {field: 'serialNumber', title: 'Numer seryjny'},
+            {field: 'faultDescription', title: 'Opis usterki'},
+            {
+                field: 'dateOfAdd', title: 'Data przyjęcia', render: dataRow => <>{
+                    moment(dataRow.dateOfAdd)
+                        .locale("pl")
+                        .format("YYYY-MM-DD HH:mm")}</>
+            },
+            {
+                field: 'status', title: 'Status', render: dataRow => {
+                    switch (dataRow.status) {
+                        case "new":
+                            return (<>Przyjęto do serwisu</>);
+                        case "start":
+                            return (<>Rozpoczęto</>);
+                        case "expertise":
+                            return (<>Ekspertyza</>);
+                        case "repair start":
+                            return (<>>W trakcie naprawy</>);
+                        case ("waiting spare parts" || "waiting forwarded"):
+                            return (<>Oczekuje</>);
+                        case "repair end":
+                            return (<>Naprawa zakończona</>);
+                        case "end":
+                            return (<>Odebrane</>);
+                        default:
+                            return (<>{dataRow.status}</>);
+                    }
+                }
+            }
+        ],
+
+        localization: {
+            pagination: {
+                labelDisplayedRows: "{from}-{to} z {count}",
+                labelRowsSelect: "wyników na stronę",
+                firstTooltip: "Pierwsza strona",
+                previousTooltip: "Poprzednia strona",
+                nextTooltip: "Następna strona",
+                lastTooltip: "Ostatnia strona"
+
+            },
+            toolbar: {
+                searchPlaceholder: "Szukaj",
+                searchTooltip: "Szukaj"
+
+            }
+        },
+        options: {
+            emptyRowsWhenPaging: false,
+            cellStyle: {
+                padding: "0px",
+                fontSize: "15px",
+                height: "23px",
+                paddingLeft: "10px"
+            },
+            tableLayout: "fixed"
+        },
     };
 
 
-    useEffect(() => {
-        dispatch(getAllRepairs())
-    }, [dispatch]);
-
-    useEffect(() => {
-        leftMenuActions.setLeftMenu([...leftMenuItems]);
-    }, []);
-
-    const repairs = useSelector(state => state.repairs);
-
-    if (repairs.length === 0) return (<>Loading...</>);
-
     return (
-        //todo:
-        //- Edycja naprawy
-        //- Statusy napraw
-        //- Możliwość rejestrowania napraw online przez klienta
-        //- Możliwość obserwacji statusu naprawy przez klienta po przez podanie np. numeru tel i imienia albo numeru seryjnego
-        //- Dołączanie zdjęć do zlecenia
-
-        // -dołączanie dokumentacji do bazy modeli aby zawsze można było w zleceniu sobie ją ściągnąć
-        // - możliość opłacenia naprawy przez klienta
-        // - dodatek przy opłacaniu "A może jeszcze..." gdzie do ostateczneij kwoty naprawy można dokupić jakąś zużytą gałkę
-        <div>
-            <Repairs2/>
-
-            {/*Aktualne naprawy:*/}
-            {/*<Paper>*/}
-            {/*    <TableContainer>*/}
-            {/*        <Table stickyHeader aria-label="sticky table">*/}
-            {/*            <TableHead>*/}
-            {/*                <TableRow>*/}
-            {/*                    {columns.map((column) => (*/}
-            {/*                        <TableCell*/}
-
-            {/*                            key={column.id}*/}
-            {/*                            align={column.align}*/}
-            {/*                            style={{minWidth: column.minWidth}}*/}
-            {/*                        >*/}
-            {/*                            {column.label}*/}
-
-
-            {/*                        </TableCell>*/}
-            {/*                    ))}*/}
-            {/*                </TableRow>*/}
-            {/*            </TableHead>*/}
-            {/*            <TableBody>*/}
-            {/*                {repairs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((repair) => {*/}
-            {/*                    return (*/}
-            {/*                        <TableRow hover*/}
-            {/*                                  role="checkbox"*/}
-            {/*                                  tabIndex={-1}*/}
-            {/*                                  key={repair.repairId}*/}
-            {/*                                  onClick={() => handleRapairClick(repair.repairId)}>*/}
-            {/*                            {columns.map((column) => {*/}
-            {/*                                switch (column.id) {*/}
-            {/*                                    case "model":*/}
-            {/*                                        return (*/}
-            {/*                                            <TableCell key={column.id}*/}
-            {/*                                                       align={column.align}>{repair.device.model}</TableCell>);*/}
-            {/*                                    case "manufacturer":*/}
-            {/*                                        return (*/}
-            {/*                                            <TableCell key={column.id}*/}
-            {/*                                                       align={column.align}>{repair.device.manufacturer}</TableCell>);*/}
-            {/*                                    case "dateOfAdd":*/}
-            {/*                                        return (*/}
-            {/*                                            <TableCell key={column.id}*/}
-            {/*                                                       align={column.align}>{moment(repair.device.dateOfAdd)*/}
-            {/*                                                .locale("pl")*/}
-            {/*                                                .format("YYYY-MM-DD HH:mm")}</TableCell>);*/}
-            {/*                                    case "status":*/}
-            {/*                                        if (repair.status === "new") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Przyjęte do serwisu</TableCell>);*/}
-
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "start") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Rozpoczęto</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "expertise") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Ekspertyza</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "repair start") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>W trakcie naprawy</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "waiting spare parts" || repair.status === "waiting forwarded") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Oczekuje</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "repair end") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Naprawa zakończona</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                        if (repair.status === "end") {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Odebrane</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                            else {*/}
-            {/*                                            return (*/}
-            {/*                                                <TableCell key={column.id}*/}
-            {/*                                                           align={column.align}>Nieznany</TableCell>);*/}
-            {/*                                        }*/}
-            {/*                                    default:*/}
-            {/*                                        return (*/}
-            {/*                                            <TableCell key={column.id}*/}
-            {/*                                                       align={column.align}>{repair[column.id]}</TableCell>);*/}
-            {/*                                }*/}
-            {/*                            })}*/}
-            {/*                        </TableRow>*/}
-            {/*                    );*/}
-            {/*                })}*/}
-            {/*            </TableBody>*/}
-            {/*        </Table>*/}
-            {/*    </TableContainer>*/}
-            {/*    <TablePagination*/}
-            {/*        rowsPerPageOptions={[10, 25, 100, 500]}*/}
-            {/*        labelRowsPerPage={"Wyników na stronę:"}*/}
-            {/*        labelDisplayedRows={({from, to, count}) => `Wyniki ${from}-${to} z ${count}`}*/}
-            {/*        component="div"*/}
-            {/*        count={repairs.length}*/}
-            {/*        rowsPerPage={rowsPerPage}*/}
-            {/*        page={page}*/}
-            {/*        onChangePage={handleChangePage}*/}
-            {/*        onChangeRowsPerPage={handleChangeRowsPerPage}*/}
-            {/*    />*/}
-            {/*</Paper>*/}
-        </div>
+        <MaterialTable
+            icons={tableIcons}
+            title="Aktualne naprawy:"
+            columns={tableConfiguration.columns}
+            data={repairs}
+            localization={tableConfiguration.localization}
+            options={tableConfiguration.options}
+            onRowClick={(event, rowData) => handleOnRowClick(rowData.repairId)}
+        />
     );
-};
-
-
-export default Repairs;
+}
