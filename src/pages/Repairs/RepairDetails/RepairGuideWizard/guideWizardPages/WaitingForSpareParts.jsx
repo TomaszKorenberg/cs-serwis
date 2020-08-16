e16789ae936ec83e2dfbe60ba560307436aa9516
Tomasz Korenberg <info@casestudio.pl>
2020-08-16 22:40:48 +0200

    New events for user activity

--- src/pages/Repairs/RepairDetails/RepairGuideWizard/guideWizardPages/WaitingForSpareParts.jsx
+++ src/pages/Repairs/RepairDetails/RepairGuideWizard/guideWizardPages/WaitingForSpareParts.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "@material-ui/core/Button";
 import DefaultActionButtons from "../DefaultActionButtons/DefaultActionButtons";
+import Backdrop from "@material-ui/core/Backdrop/Backdrop";
+import Fade from "@material-ui/core/Fade/Fade";
+import {Modal} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 
 
-const WaitingForSpareParts = ({repairDetails}) => {
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+const WaitingForSpareParts = ({repairDetails, handleUpdateRepairData}) => {
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const classes = useStyles();
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
+
+    const handleSave = async () => {
+        await handleUpdateRepairData("status", "repair start");
+        setOpenModal(false);
+    };
+
+    const handleOpenModal = () => {
+        setOpenModal(true)
+    };
 
     return (
         <>
@@ -12,11 +47,50 @@ const WaitingForSpareParts = ({repairDetails}) => {
             - Oczekująca / Twoja naprawa oczekuje na części<br/>
             <div className={"repairActionsWrapper"}>
                 <div>
-                    <Button variant="outlined">Przyszły części</Button><br/>
-                    <Button variant="outlined">Naprawiono</Button><br/><br/>
+                    <Button variant="outlined"
+                            id={"confirmPartsDelivery"}
+                            onClick={handleOpenModal}>Dostawa części</Button><br/>
                     <DefaultActionButtons repairDetails={repairDetails}/>
                 </div>
             </div>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={openModal}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openModal}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">Dostawa części</h2>
+
+                        Potwierdzasz dostawę poniższych części? <br/><br/>
+
+                        {repairDetails.partsOrdered}<br/><br/>
+
+                        <Button
+                            fullWidth={false}
+                            disableElevation={true}
+                            onClick={() => handleSave()}
+                            variant="outlined"
+                            size={"medium"}
+                        >Tak</Button>
+                        <Button
+                            fullWidth={false}
+                            disableElevation={true}
+                            onClick={handleClose}
+                            variant="outlined"
+                            size={"medium"}
+                        >Nie</Button>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     );
 };
--- src/pages/Repairs/RepairDetails/RepairGuideWizard/guideWizardPages/WaitingForwarded.jsx
+++ src/pages/Repairs/RepairDetails/RepairGuideWizard/guideWizardPages/WaitingForwarded.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "@material-ui/core/Button";
 import DefaultActionButtons from "../DefaultActionButtons/DefaultActionButtons";
+import Backdrop from "@material-ui/core/Backdrop/Backdrop";
+import Fade from "@material-ui/core/Fade/Fade";
+import {Modal} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 
 
-const WaitingForwarded = ({repairDetails}) => {
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+const WaitingForwarded = ({repairDetails, handleUpdateRepairData}) => {
+
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const classes = useStyles();
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
+
+    const handleSave = async () => {
+        await handleUpdateRepairData("status", "repair start");
+        setOpenModal(false);
+    };
+
+    const handleOpenModal = () => {
+        setOpenModal(true)
+    };
 
     return (
         <>
@@ -12,10 +48,48 @@ const WaitingForwarded = ({repairDetails}) => {
             - Oczekująca / Przekazano do serwisu zewnętrznego (widziane tylko dla serwisu, dla klienta oczekuje na części)<br/>
             <div className={"repairActionsWrapper"}>
                 <div>
-                    <Button variant="outlined">Powrot sprzętu z serwisu</Button><br/><br/>
+                    <Button variant="outlined"
+                            id={"confirmPartsDelivery"}
+                            onClick={handleOpenModal}>Powrot sprzętu z serwisu</Button><br/><br/>
                     <DefaultActionButtons repairDetails={repairDetails}/>
                 </div>
             </div>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={openModal}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openModal}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">Powrot sprzętu z serwisu</h2>
+
+                        Potwierdzasz powrót sprzętu z serwisu zewnętrznego?<br/><br/>
+
+                        <Button
+                            fullWidth={false}
+                            disableElevation={true}
+                            onClick={() => handleSave()}
+                            variant="outlined"
+                            size={"medium"}
+                        >Tak</Button>
+                        <Button
+                            fullWidth={false}
+                            disableElevation={true}
+                            onClick={handleClose}
+                            variant="outlined"
+                            size={"medium"}
+                        >Nie</Button>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     );
 };
