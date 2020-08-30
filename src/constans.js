303c26b4c8589d6f14636c854b475a300f442016
Tomasz Korenberg <info@casestudio.pl>
2020-08-30 21:39:22 +0200

    constans file added

--- src/constans.js
+++ src/constans.js
@@ -1,3 +1,52 @@
 export const repairStatus = {
-    
-}
+    registered: {
+        info: "registered",
+        shortDescription: "Zarejestrowano",
+        longDescription: "Urządzenie zostało zarejestrowane w systemie serwisowym"
+    },
+    new: {
+        info: "new",
+        shortDescription: "Przyjęte do serwisu",
+        longDescription: "Urządzenie zostało przyjęte do serwisu"
+    },
+    start: {
+        info: "start",
+        shortDescription: "Rozpoczęto obsługę zlecenia",
+        longDescription: "Serwis rozpoczął obsługę zlecenia"
+    },
+    expertise: {
+        info: "expertise",
+        shortDescription: "Ekspertyza",
+        longDescription: "Serwis wystawił ekspertyzę i wycenił naprawę"
+    },
+    repairStart: {
+        info: "repair start",
+        shortDescription: "Rozpoczęto naprawę",
+        longDescription: "Serwis rozpoczął naprawę urządzenia"
+    },
+    waitingSpareParts: {
+        info: "waiting spare parts",
+        shortDescription: "Czeka na części",
+        longDescription: "Urządzenie oczekuje na dostawę części"
+    },
+    waitingForwarded: {
+        info: "waiting forwarded",
+        shortDescription: "Serwis zewnętrzny",
+        longDescription: "Urządzenie zostało wysłane do innego punktu serwisowego i czeka na powrót"
+    },
+    repairEnd: {
+        info: "repair end",
+        shortDescription: "Naprawa zakończona",
+        longDescription: "Urządzenie zostało naprawione i oczekuje na odbiór lub wysyłkę"
+    },
+    end: {
+        info: "end",
+        shortDescription: "Odebrane",
+        longDescription: "Urządzenie zostało odebrane lub wysłane"
+    },
+    archive: {
+        info: "archive",
+        shortDescription: "Archiwalne",
+        longDescription: "Zlecenie archiwalne"
+    },
+};
