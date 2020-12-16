32bd3429bbca3587b72a5f27c0c919ca1c851440
Tomasz Korenberg <info@casestudio.pl>
2020-12-16 01:02:39 +0100

    added default time to useDebounce hook

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const useDebounce = () => {
     const [typingTimeout, setTypingTimeout] = useState();
-    return (action, waitTime) => {
+    return (action, waitTime = 1000) => {
         clearTimeout(typingTimeout);
         const timeout = setTimeout(() => action(), waitTime);
         setTypingTimeout(timeout);
